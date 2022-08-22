7f761db6a8163c6487ef99d868254d137f890717
Marcin Pierzchała <pierzchala.ma@gmail.com>
2022-08-23 00:02:31 +0200

    test automatyzacji

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 function render():JSX.Element{
     return (
-        <div className="flex w-full bg-red-400 h-10 sticky top-0">
+        <div className="flex w-full bg-green-400 h-10 sticky top-0">
             
         </div>
     );
