56592cfa356032b7bd07e975e2d5411b4bae5a7f
Marcin Pierzchała <pierzchala.ma@gmail.com>
2022-11-07 18:33:49 +0100

    AWSDZ-38 added landing page

--- pwa/src/components/Header.tsx
+++ pwa/src/components/Header.tsx
@@ -1,31 +1,33 @@
+import { Link } from "react-router-dom";
+
 function render(): JSX.Element {
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="/" className="flex items-center">
+          <Link to="/" className="flex items-center">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="mr-3 h-6 sm:h-9"
               alt="Flowbite Logo"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Schronisko
             </span>
-          </a>
+          </Link>
           <div className="flex items-center lg:order-2">
-            <a
-              href="/login"
+            <Link
+              to="/login"
               className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Logowanie
-            </a>
-            <a
-              href="/register"
+            </Link>
+            <Link
+              to="/register"
               className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
             >
               Rejestracja
-            </a>
+            </Link>
           </div>
         </div>
       </nav>
