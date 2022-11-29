53cd442c90f9345bf51d10d7fa06024c110aadbb
zimniok <indiana.snake@gmail.com>
2022-11-29 19:09:16 +0100

    AWSDZ-55 added file adding to animal form

--- pwa/src/components/AddAnimal.tsx
+++ pwa/src/components/AddAnimal.tsx
@@ -4,26 +4,39 @@ import axiosInstance from "../utils/axiosInstance";
 import useToken from "../utils/useToken";
 import PrimaryButton from "./PrimaryButton";
 import UploadComponent from "./UploadImage";
+import { urlToFile } from "../utils/imgUtils";
 
 function AddAnimal() {
     const { token } = useToken();
     const [pictures, setPictures] = useState<Array<any>>([]);
 
-    const [startDate, setStartDate] = useState(new Date());
-
     function handleChange(files: Array<any>) {
         const picturesTemp = pictures;
 
-        setPictures([...picturesTemp, pictures])
+        setPictures(files)
     };
 
-    function handleSubmit(data: any) {
-        console.log(data);
+    async function handleSubmit(data: any) {
         axiosInstance.post("animals", data, { headers: { 'Authorization': `Bearer ${token}` } })
-            .then((response) => console.log(response))
+            .then((response) => {
+                console.log(response)
+
+                pictures.map((picture, index) => {
+                    urlToFile(picture, index.toString())
+                        .then((file) => {
+                            console.log('plik2')
+                            console.log(file);
+                            axiosInstance.post("media_objects", { file: file, domain: `animal/${response.data.id}` }, { headers: { 'Authorization': `Bearer ${token}`, "Content-Type": "multipart/form-data" } })
+                                .then((response) => console.log(response));
+                        });
+
+                })
+            })
             .catch((error) => {
                 console.log(error);
-            });;
+                alert("Coś poszło nie tak. Spróbuj ponownie.");
+                return;
+            });
     }
 
     return (
