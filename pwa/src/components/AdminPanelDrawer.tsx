e298df28b4273c493e170bbab54b8667a0e8842d
zimniok <mpierzchala@bee2code.com>
2022-12-13 12:32:40 +0100

    AWSDZ-78 modified admin panel view

--- pwa/src/components/AdminPanelDrawer.tsx
+++ pwa/src/components/AdminPanelDrawer.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function render(props: any){
-    return <div>drawer</div>
+    return (
+        <div className="flex flex-col h-100 min-h-screen bg-gray-700 text-white">
+            <Link to="edit_landing_page" className="p-3">Edytuj stronę główną</Link>
+            <Link to="add_animal" className="p-3">Dodaj zwierzę</Link>
+            <Link to="adoptions" className="p-3">Lista adopcji</Link>
+            <Link to="add_adoption" className="p-3">Dodaj adopcję</Link>
+        </div>
+    )
 }
 
 export default render;
