0b888d1923f6271135152f8dcbc14ad7c5891cad
zimniok <mpierzchala@bee2code.com>
2022-12-18 23:06:55 +0100

    title color fix

--- pwa/src/components/Header.tsx
+++ pwa/src/components/Header.tsx
@@ -34,7 +34,7 @@ function Header(): JSX.Element {
               className="mr-3 h-6 sm:h-9"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-white">
               Schronisko
             </span>
           </Link>
